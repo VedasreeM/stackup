117b5b13a15d2975d6c06e2eca6ae8e666519f4e
Vedasree M <126886440+VedasreeM@users.noreply.github.com>
2023-11-11 13:56:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
-function addTask() {
-    // Get the task input element
-    var taskInput = document.getElementById('newTask');
-
-    // Get the task list element
-    var taskList = document.getElementById('taskList');
 document.addEventListener('DOMContentLoaded', function () {
-    const todos = []; // Replace with actual data from your database
+    const todos = [];
 
     const todoList = document.getElementById('todos');
     const todoInput = document.getElementById('todoInput');
@@ -41,7 +35,10 @@ document.addEventListener('DOMContentLoaded', function () {
 
     function deleteTodo(todoId) {
         // Add logic to delete the todo based on its ID
-        console.log(`Delete todo with ID ${todoId}`);
+        const updatedTodos = todos.filter(todo => todo.id !== todoId);
+        todos.length = 0; // Clear the existing array
+        Array.prototype.push.apply(todos, updatedTodos); // Push updated todos back
+        renderTodos();
     }
 
     document.querySelector('form').addEventListener('submit', function (event) {
@@ -51,46 +48,3 @@ document.addEventListener('DOMContentLoaded', function () {
 
     renderTodos();
 });
-
-    // Create a new list item element
-    var listItem = document.createElement('li');
-
-    // Create a text node with the task input value
-    var taskText = document.createTextNode(taskInput.value);
-
-    // Create a "Mark as Complete" button element
-    var completeButton = document.createElement('button');
-    completeButton.appendChild(document.createTextNode('Mark'));
-    completeButton.className = 'complete-button';
-    // Add an event listener to mark the task as complete when the button is clicked
-    completeButton.addEventListener('click', function () {
-        listItem.classList.toggle('completed');
-    });
-
-
-    var deleteButton = document.createElement('button');
-    deleteButton.appendChild(document.createTextNode('Delete'));
-
-    deleteButton.className = 'delete-button';
-    deleteButton.addEventListener('click', function () {
-        taskList.removeChild(listItem);
-    });
-
-    // Append taskText and completeButton to the list item
-    
-    listItem.appendChild(completeButton);
-    listItem.appendChild(taskText);
-    listItem.appendChild(deleteButton);
-    taskList.appendChild(listItem);
-    // Append the list item to the task list
-    taskList.appendChild(listItem);
-
-    // Clear the task input field
-    taskInput.value = '';
-}
-
-function clearAllTasks() {
-    var taskList = document.getElementById('taskList');
-    taskList.innerHTML = ''; // Remove all tasks
-}
-
