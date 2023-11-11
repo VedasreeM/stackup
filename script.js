f5a988fb1291e0294b899b38b7328cc4dcfb2217
Vedasree M <126886440+VedasreeM@users.noreply.github.com>
2023-11-11 13:47:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,53 @@ function addTask() {
 
     // Get the task list element
     var taskList = document.getElementById('taskList');
+document.addEventListener('DOMContentLoaded', function () {
+    const todos = []; // Replace with actual data from your database
+
+    const todoList = document.getElementById('todos');
+    const todoInput = document.getElementById('todoInput');
+
+    function renderTodos() {
+        todoList.innerHTML = '';
+        todos.forEach(todo => {
+            const li = document.createElement('li');
+            li.innerHTML = `
+                <span>${todo.text}</span>
+                <button onclick="updateTodo(${todo.id})">Update</button>
+                <button onclick="deleteTodo(${todo.id})">Delete</button>
+            `;
+            todoList.appendChild(li);
+        });
+    }
+
+    function addTodo() {
+        const newTodo = {
+            id: todos.length + 1,
+            text: todoInput.value,
+            completed: false,
+        };
+        todos.push(newTodo);
+        todoInput.value = '';
+        renderTodos();
+    }
+
+    function updateTodo(todoId) {
+        // Add logic to update the todo based on its ID
+        console.log(`Update todo with ID ${todoId}`);
+    }
+
+    function deleteTodo(todoId) {
+        // Add logic to delete the todo based on its ID
+        console.log(`Delete todo with ID ${todoId}`);
+    }
+
+    document.querySelector('form').addEventListener('submit', function (event) {
+        event.preventDefault();
+        addTodo();
+    });
+
+    renderTodos();
+});
 
     // Create a new list item element
     var listItem = document.createElement('li');
